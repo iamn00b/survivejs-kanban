ab0a7b96bbe2a2db9defcc4045efecc52a88d513
Thirafi Dide <jurnaldide@gmail.com>
2016-02-01 18:45:32 +0700

    add live reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,48 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
-	app: path.join(__dirname, 'app'),
-	build: path.join(__dirname, 'build')
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
 };
 
 const common = {
-	entry: PATHS.app,
-	output: {
-		path: PATHS.build,
-		filename: 'bundle.js'
-	}
+    entry: PATHS.app,
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    }
+}
+
+const devConfiguration = {
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
 }
 
 if (TARGET === 'start' || !TARGET) {
-	module.exports = merge(common, {});
+    module.exports = merge(common, devConfiguration);
 }
 
 if (TARGET === 'build') {
-	module.exports = merge(common, {});
+    module.exports = merge(common, {});
 }
