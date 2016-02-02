608e85fa461b4490df8cd1c485886274e6e2ea71
Thirafi Dide <jurnaldide@gmail.com>
2016-02-02 17:49:27 +0700

    decouple Notes from App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,6 @@
 import uuid from 'node-uuid'; 
 import React from 'react';
-import Note from './Note';
+import Notes from './Notes';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -30,13 +30,7 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <ul>
-          {notes.map(note =>
-            <li key={note.id}>
-              <Note task={note.task} />
-            </li>
-          )}
-        </ul>
+        <Notes notes={notes} />
       </div>
     );
   }
