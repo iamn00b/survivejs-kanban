6a56551a55b912560149acaad9063469e65d2a1d
Thirafi Dide <jurnaldide@gmail.com>
2016-02-03 12:32:29 +0700

    add Note Store

---
+++ app/stores/NoteStore.js
@@ -0,0 +1,41 @@
+import uuid from 'node-uuid';
+import alt from '../libs/alt';
+import NoteActions from '../actions/NoteActions';
+
+class NoteStore {
+  constructor() {
+    this.bindActions(NoteActions);
+
+    this.notes = [];
+  }
+
+  create(note) { 
+    note.id = uuid.v4();
+
+    const notes = this.notes.concat(note);
+
+    this.setState({ notes });
+  }
+
+  update(updatedNote) { 
+    const notes = this.notes.map(note => {
+      if (note.id === updatedNote.id) {
+        return Object.assign({}, note, updatedNote);
+      }
+
+      return note;
+    });
+
+    this.setState({ notes });
+
+  }
+
+  delete(id) { 
+    const notes = this.notes.filter(note => note.id !== id);
+
+    this.setState({ notes });
+  }
+
+}
+
+export default alt.createStore(NoteStore, 'NoteStore');
