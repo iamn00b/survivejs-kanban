6e00c3d9049aae94635510367a3fb2943d8662a8
Thirafi Dide <jurnaldide@gmail.com>
2016-02-04 22:46:30 +0700

    simplifying App.jsx with alt container

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,38 +1,29 @@
 import React from 'react';
+import AltContainer from 'alt-container';
 import Notes from './Notes';
 
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = NoteStore.getState();
-  }
-
-  componentDidMount() {
-    NoteStore.listen(this.storeChanged);
-  }
-
-  componentWillUnmount() {
-    NoteStore.unlisten(this.storeChanged);
-  }
-
-  storeChanged = (state) => {
-    this.setState(state);
-  };
 
   render() { 
-    const notes = this.state.notes;
-
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <Notes 
-          notes={notes} 
-          onEdit={this.editNote}
-          onDelete={this.deleteNote} />
+
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getState().notes
+          }} >
+
+            <Notes 
+              onEdit={this.editNote}
+              onDelete={this.deleteNote} />
+
+        </AltContainer>
+
       </div>
     );
   }
